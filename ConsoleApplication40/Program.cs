7f281cda079bebaa718208138acfaf072326d66f
daryaleontyeva <36742645+daryaleontyeva@users.noreply.github.com>
2018-04-10 13:45:00 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication40/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hashable
+{
+    public class HashTable
+    {
+
+        private KeyValuePair[] _array;
+        private int currentOffset;
+        public HashTable(int size)
+        {
+            _array = new KeyValuePair[size];
+        }
+        public void PutPair(object key, object value)
+        {
+
+            var hashCode = key.GetHashCode();
+            var bucketNumber = GetBucketNumber(key);
+
+            var bucket = _array[bucketNumber];
+            while (bucket != null)
+            {
+                if (Equals(bucket.Key, key))
+                {
+                    bucket.Value = value;
+                    return;
+                }
+                bucket = bucket.Next;
+            }
+
+            var writeBucket = _array[bucketNumber];
+            if (writeBucket == null)
+            {
+                _array[bucketNumber] = new KeyValuePair { Value = value, Key = key };
+            }
+            else
+            {
+                while (writeBucket.Next != null)
+                {
+                    writeBucket = writeBucket.Next;
+                }
+                writeBucket.Next = new KeyValuePair { Value = value, Key = key };
+            }
+
+        }
+        private int GetBucketNumber(object key)
+        {
+            var hashCode = key.GetHashCode();
+            var bucketNumber = Math.Abs(hashCode) % _array.Length;
+            return bucketNumber;
+        }
+        public object GetValueByKey(object key)
+        {
+
+            var bucketNumber = GetBucketNumber(key);
+            var bucket = _array[bucketNumber];
+            while (bucket != null)
+            {
+                if (Equals(bucket.Key, key))
+                {
+                    return bucket.Value;
+                }
+                bucket = bucket.Next;
+            }
+            return null;
+
+        }
+        class KeyValuePair
+        {
+            public Object Key { get; set; }
+            public Object Value { get; set; }
+            public KeyValuePair Next { get; set; }
+        }
+    }
+}
---
+++ TestOfProgr/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hashable;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var hashtable = new HashTable(3);
+            hashtable.PutPair("key1", "value1");
+            hashtable.PutPair("key2", "value2");
+            hashtable.PutPair("3", "value3");
+
+            var value = hashtable.GetValueByKey("key1");
+            Assert.IsTrue("value1" == (string)value);
+            value = hashtable.GetValueByKey("key2");
+            Assert.IsTrue("value2" == (string)value);
+            value = hashtable.GetValueByKey("3");
+            Assert.IsTrue("value3" == (string)value);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var hashtable = new HashTable(3);
+            hashtable.PutPair("key1", "value1");
+            hashtable.PutPair("key1", "value2");
+
+
+            var value = hashtable.GetValueByKey("key1");
+            Assert.AreEqual((string)value, "value2");
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var hashtable = new HashTable(10000);
+            for (int i = 0; i < 10000; i++)
+            {
+                hashtable.PutPair("key" + i, "value" + i);
+            }
+            var value = hashtable.GetValueByKey("key8888");
+            Assert.AreEqual((string)value, "value8888");
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var hashtable = new HashTable(10000);
+            for (int i = 0; i < 10000; i++)
+            {
+                hashtable.PutPair("key" + i, "value" + i);
+            }
+            for (int i = 0; i < 1000; i++)
+            {
+                Assert.AreEqual(hashtable.GetValueByKey(i), null);
+            }
+
+
+        }
+    }
+}
